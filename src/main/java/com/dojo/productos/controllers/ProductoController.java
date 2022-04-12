979f09d1dacac934a0d604591c1b62ab897022a8
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-04-12 14:20:28 -0500

    Modificado archivo properties, model Producto y controller Producto. Estado: no ejecuta correctamente. En proceso de solución

--- src/main/java/com/dojo/productos/controllers/ProductoController.java
+++ src/main/java/com/dojo/productos/controllers/ProductoController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api")
 public class ProductoController {
--- src/main/java/com/dojo/productos/models/Producto.java
+++ src/main/java/com/dojo/productos/models/Producto.java
@@ -3,7 +3,7 @@ package com.dojo.productos.models;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(collection = "producto")
 public class Producto {
     
     @Id
