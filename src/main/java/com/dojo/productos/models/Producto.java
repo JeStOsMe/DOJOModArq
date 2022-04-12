be68878ab969876ef3fb424fcdb0a8807b9fed5c
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-04-12 13:17:09 -0500

    POM modificado: agregada dependencia MongoDB. application.properties modificado: agregado string de conexión a Mongo Atlas. Model Producto terminado

---
+++ src/main/java/com/dojo/productos/models/Producto.java
@@ -0,0 +1,52 @@
+package com.dojo.productos.models;
+
+import org.springframework.data.annotation.Id;
+
+public class Producto {
+    
+    @Id
+    private String id;
+
+    private String nombre;
+
+    private Long precio;
+
+    public Producto(String nombre, Long precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public Producto() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Long getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Long precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto [id=" + id + ", nombre=" + nombre + ", precio=" + precio + "]";
+    }
+
+    
+}
